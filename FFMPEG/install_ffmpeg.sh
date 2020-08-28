1117b1b5f73e33284550f468d68b57b36ed0b9c4
Reasno <guxi99@gmail.com>
2020-08-28 16:07:51 +0800

    add ffmpeg

--- FFMPEG/install_ffmpeg.sh
+++ FFMPEG/install_ffmpeg.sh
@@ -2,7 +2,7 @@
 apt-get update
 apt-get install -y git build-essential gcc make yasm autoconf automake cmake libtool checkinstall wget software-properties-common pkg-config libmp3lame-dev libunwind-dev zlib1g-dev
 cd ~/
-wget https://www.ffmpeg.org/releases/ffmpeg-4.1.tar.gz
+curl -sL https://www.ffmpeg.org/releases/ffmpeg-4.1.tar.gz
 tar -xzf ffmpeg-4.1.tar.gz
 cd ffmpeg-4.1
 ./configure --enable-gpl --enable-libmp3lame --enable-decoder=mjpeg,png --enable-encoder=png
